72bc780c5603f15ca4a300b8448ffd74ab58d884
Gabriele Sampognaro <54634440+giogab95@users.noreply.github.com>
2019-09-15 19:01:55 +0200

    Updates

---
+++ precache-manifest.ea1f3303198564fb520b44b2b2074491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f70c4d15ad13abc669ba5a19d8f382f",
+    "url": "/search-type-apis/index.html"
+  },
+  {
+    "revision": "f185f8b36fce8f4bfb83",
+    "url": "/search-type-apis/static/css/main.1e28b61c.chunk.css"
+  },
+  {
+    "revision": "6bc6059746e9fdb798a2",
+    "url": "/search-type-apis/static/js/2.5a9977e9.chunk.js"
+  },
+  {
+    "revision": "f185f8b36fce8f4bfb83",
+    "url": "/search-type-apis/static/js/main.1fde7c09.chunk.js"
+  },
+  {
+    "revision": "d9a7ac6a5681cd24af13",
+    "url": "/search-type-apis/static/js/runtime~main.cfdd64b6.js"
+  },
+  {
+    "revision": "4d6b805f6d2c800958a9a307be87c7d1",
+    "url": "/search-type-apis/static/media/Pacman-1s-200px.4d6b805f.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/search-type-apis/static/media/logo.5d5d9eef.svg"
+  }
+]);
